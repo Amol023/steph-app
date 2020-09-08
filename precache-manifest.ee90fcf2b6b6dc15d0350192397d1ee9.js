6182345e940c2fb192de980013b36ba68f9e3067
Amol Joshi <amoljoshi@Amols-MacBook-Air.local>
2020-09-07 17:23:42 -0700

    Updates

---
+++ precache-manifest.ee90fcf2b6b6dc15d0350192397d1ee9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0577b4989a5d66636db57d20ee8da468",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1065444a7a5036319d6c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cafb3872ec9fdc895fc9",
+    "url": "/static/js/2.ccecd96c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ccecd96c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1065444a7a5036319d6c",
+    "url": "/static/js/main.92f4fb07.chunk.js"
+  },
+  {
+    "revision": "b2d96956173856a17286",
+    "url": "/static/js/runtime-main.847d7fe3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
